9191c6ffe85166c4f85b6ac2150e7845d69f661c
biswashniyam <biswashdevkota2023@gmail.com>
2024-02-20 20:23:48 +0545

    integer

--- day1/second.cpp
+++ day1/second.cpp
@@ -2,13 +2,36 @@
 using namespace std;
 int main()
 {
+    /*
     //integer operation
     int a=10;
     int b=20;
     cout<<"sum is :"<<a+b<<endl;
     cout<<"Diffirence is:"<<b-a<<endl;
     cout<<"Multiplication is :"<<a*b<<endl;
-    cout<<"Division is :"<<b/a<<endl;
+    cout<<"Division is :"<<b/a<<endl;*/
+
+    int a=123;
+    cout<<a<<endl;
+
+    char b ='v';
+    cout<<b<<endl;
+
+    bool bl= true;
+    cout<<bl<<endl;
+
+    float f=0.3222;
+    cout<<f<<endl;
+
+    double doi=1.23;
+    cout <<doi<<endl;
+
+    int sizey=sizeof(doi);
+    cout<<"size of doi is :"<<sizey<<endl;
+
+    
+
+
     
 
 
