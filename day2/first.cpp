989fa67df0eec08e896a8bf0e127fdb66c18e1f7
biswashniyam <biswashdevkota2023@gmail.com>
2024-02-21 22:31:33 +0545

    scondcommit

--- day2/first.cpp
+++ day2/first.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     //conditional statements
-    int n;
+    /*int n;
     cout<<"enter the value of n:"<<endl;
     cin>>n;
     if(n>0)
@@ -19,5 +19,23 @@ int main()
     else{
         cout<<"it is less than zero"<<endl;
     }
+    int a,b;
+    cout<<"ente rthe value of a and b"<<endl;
+    cin>>a>>b;
+    cout<<"Value of a and b is :"<<a<<"and"<<b<<endl;*/
+    //cin donot reads (space),(tab),(enter\n)
+    //for this we use cin.get();
+
+    int a,b;
+    cout<<"Enter the value of a is :"<<endl;
+    a=cin.get();//it picks up character if 1 is input it picks ASCII value of 1
+    cout<<"Value of a is :"<<a<<endl;//(2  2)=50
+    /*
+Enter the value of a is :
+1
+Value of a is :49*/
+//we can take input as space ,tab,enter by using a cin.get();
+
+
 
 }
