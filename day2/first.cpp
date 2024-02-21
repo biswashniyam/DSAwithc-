c2e5a98fdf6bb3fd9bdc243b11d926b51e95eb30
biswashniyam <biswashdevkota2023@gmail.com>
2024-02-21 22:17:07 +0545

    first commit of day2

---
+++ day2/first.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //conditional statements
+    int n;
+    cout<<"enter the value of n:"<<endl;
+    cin>>n;
+    if(n>0)
+    {
+        cout<<"Input no is greater than zero"<<endl;
+
+    }
+    else if(n>=0)
+    {
+        cout<<"input is equal to zero"<<endl;
+    }
+    else{
+        cout<<"it is less than zero"<<endl;
+    }
+
+}
