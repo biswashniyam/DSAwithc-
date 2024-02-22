20d4ddf68acf8a05a52d3dd6928414207d91b43e
biswashniyam <biswashdevkota2023@gmail.com>
2024-02-22 20:09:33 +0545

    decond commit of day2

--- day3/first.cpp
+++ day3/first.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
+    /*
     char ch;
     cout<<"Enter the character::"<<endl;
     cin>>ch;
@@ -20,4 +21,31 @@ int main()
         cout<<"It lies inside the range of 0 to 9"<<endl;
     }
     return 0;
+    */
+   /*int n;
+   cout<<"Enter the value of n to run loop nth times:"<<endl;
+   cin>>n;
+   int i=0;
+   while( i <= n)
+   {
+    cout<<""<<endl;
+    cout<<i<<endl;
+    i++;
+    
+
+   }
+   return 0;*/
+
+   int a;
+   cout<<"Enter the value of a:"<<endl;
+   cin>>a;
+   int sum=0;
+   int i=0;
+   while(i<=a)
+   {
+    sum=sum+i;
+    i++;
+   }
+   cout<<"Sum is :"<<sum<<endl;
+   return 0;
 }
