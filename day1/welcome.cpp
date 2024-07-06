1241eb410e9b953da3c49ab98be33b3d58232c99
biswashniyam <biswashdevkota2023@gmail.com>
2024-07-06 11:21:38 +0545

    removed previous folder

--- day1/welcome.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"Namaste nepal this is day 1 of c++"<<endl;
-}
--- day2/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-int a,b;
-    cout<<"Enter the value of a is :"<<endl;
-    a=cin.get();
-    cout<<"Value of a is :"<<a<<endl;
